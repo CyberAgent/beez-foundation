1f23a64167fec0771641efb7dae1f494c3bce9d2
HIRAKI Satoru <saruko313@gmail.com>
2015-02-24 12:52:43 +0900

    fix example js

--- example/js/local/release.js
+++ example/js/local/release.js
@@ -1,13 +1,15 @@
-{
-    "release": { // enviroment name
-        "stylus": {
-            "options": { // @see http://learnboost.github.io/stylus/docs/executable.html
-                "encode": "utf-8",
-                "compress": true,
-                "firebug": false,
-                "linenos": false,
-                "nib": true // stylus nib on/of
+(function () {
+    return {
+        "release": { // enviroment name
+            "stylus": {
+                "options": { // @see http://learnboost.github.io/stylus/docs/executable.html
+                    "encode": "utf-8",
+                    "compress": true,
+                    "firebug": false,
+                    "linenos": false,
+                    "nib": true // stylus nib on/of
+                }
             }
         }
-    }
-}
+    };
+}());
